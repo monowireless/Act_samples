c75c51f07020433c89dcb428a7e7ece6f5624cf4
monowireless <monowireless-noreply@mono-wireless.com>
2020-09-17 11:22:21 +0900

    update act4 according to web tutorial page.

--- act4/act4.cpp
+++ act4/act4.cpp
@@ -1,30 +1,29 @@
 #include <TWELITE>
 
-const uint8_t PIN_DO1 = 18;
-const uint8_t PIN_DI1 = 12;
+const uint8_t PIN_LED = 18;
+const uint8_t PIN_BUTTON = 12;
 
-/*** the setup procedure (called on boot) */
 void setup() {
-    pinMode(PIN_DO1, OUTPUT);
-    pinMode(PIN_DI1, INPUT_PULLUP); 
+    pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_BUTTON, INPUT_PULLUP); 
 
     Buttons.setup(5);
-    Buttons.begin(1UL << PIN_DI1, 5, 10);
+    Buttons.begin(1UL << PIN_BUTTON, 5, 10);
 
-    Serial << "--- act4 (button&LED) ---" << crlf;
+    Serial << "--- act4 (button&LED)  ---" << crlf;
 }
 
-/*** the loop procedure (called every event) */
+/*** loop procedure (called every event) */
 void loop() {
     if (Buttons.available()) {
         uint32_t bm, cm;
         Buttons.read(bm, cm);
 
         if (bm & (1UL << 12)) {
-            digitalWrite(PIN_DO1, HIGH);
+            digitalWrite(PIN_LED, HIGH);
             Serial << "Button Released!" << crlf;
         } else {
-            digitalWrite(PIN_DO1, LOW);
+            digitalWrite(PIN_LED, LOW);
             Serial << "Button Pressed!" << crlf;
         }
     }
